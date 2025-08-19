71973a5fe8bca92647923fe14aa5df0e65b0ec0b
ElioGlock <eliosilva12356@gmail.com>
2025-08-19 09:12:02 -0300

    App strucutre start

--- projetos/web/newss/news.py
+++ projetos/web/newss/news.py
@@ -13,49 +13,113 @@ from pytimedinput import timedInput
 class App_News:
     def __init__(self):
         self.dict_site = {}
-        self.all_sites = ["cnn","infomoney","bbc"]
+        self.all_sites = ["cnn_news","cnn_tech","infomoney","bbc_news","bbc_tech"]
+
         self.screen = 0
+        self.kill = False
         
         self.news = self._read_file("news") if "news" in os.listdir() else []
         self._update_file(self.news,"news")
         self.sites = self._read_file("sites") if "sites" in os.listdir() else []
+        self._update_file(self.sites,"sites")
+        
+        for site in self.all_sites:
+            self.dict_site[site] = Site(site)
+        
+        self.news_thread = Thread(target=self.update_news, daemon=True)
+        self.news_thread.start()
         
     def _update_file(self,lista,mode= "news"): #cria um arquivo pra salvar a lista
         with open(mode, "wb") as fp:
             pickle.dump(lista,fp)   
         
     def _read_file(self,mode= "news"):  # lê o arquivo criado acima e serve para armazenar dados
-        with open(mode, "wb") as fp:
+        with open(mode, "rb") as fp:
             n_list = pickle.load(fp)
             return n_list
+    def update_news(self):
+        
+        while not self.kill:
+            print("update")
+            for site in self.all_sites:
+                self.dict_site[site].update_news()
+                for key,value in self.dict_site[site].news.items():
+                    dict_aux ={}
+                    dict_aux["data"] = datetime.now()
+                    dict_aux["fonte"] = site
+                    dict_aux["materia"] = key
+                    dict_aux["link"] = value
+                    if len(self.news) == 0:
+                        self.news.insert(0,dict_aux)
+                        continue
+                    add_news = True
+                    for news in self.news:
+                        if dict_aux["materia"] == news["materia"] and dict_aux["fonte"] == news["fonte"]:
+                            add_news = False
+                            break
+                    if add_news:
+                         self.news.insert(0,dict_aux)
+            self.news = sorted(self.news, key =lambda d:d["data"], reverse= True)
+            self._update_file(self.news,"news")
+           
+            time.sleep(5)       
     
+    def windows(self):
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        icon_path = os.path.join(script_dir, "images", "icon.png")
 
+        self.window = tk.Tk()
+        self.window.title("News")
+        self.window.geometry("500x400")
+        icon = tk.PhotoImage(file=icon_path)
+        self.window.iconphoto(True,icon)
+        self.window.config(background="#364153")
+        while True:
+            os.system("cls")
+            match self.screen:
+                    case 0:
+                        title = tk.Label(
+                        self.window,
+                        text="Today's News",
+                        anchor="center",
+                        font=("Helvetica",18),
+                        foreground="white",
+                        background="#364153")
+                        title.pack(ipady=10)
+                        
+                        button_bbc_news = tk.Button(
+                            self.window,text="bbc_news",
+                            )
+                        button_bbc_news.pack()
+                    case 1:
+                        title = tk.Label(
+                        self.window,
+                        text="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                        anchor="center",
+                        font=("Helvetica",18),
+                        foreground="white",
+                        background="#364153")
+                        title.pack(ipady=10)
+                        pass
+                    case 2:
+                        pass
+                    case 3:
+                        pass
+            
+            self.window.mainloop() 
+        
+            
+                    
+                
+            
 self = App_News()
-
-
-# path
-# script_dir = os.path.dirname(os.path.abspath(__file__))
-# icon_path = os.path.join(script_dir, "images", "icon.png")
-
-# window = tk.Tk()
-# window.title("News")
-# window.geometry("500x400")
-# icon = tk.PhotoImage(file=icon_path)
-# window.iconphoto(True,icon)
-# window.config(background="#364153")
-
-# title = tk.Label(
-#     window,
-#     text="Today's News",
-#     anchor="center",
-#     font=("Leoscar",18),
-#     foreground="white",
-#     background="#364153",
-
-#     )
-    
-# title.pack(ipady=10)
+self.windows()
+# self.update_news()
 
 
 
-# window.mainloop()
+
+
+
+
+
--- projetos/web/newss/script.py
+++ projetos/web/newss/script.py
@@ -2,116 +2,111 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from urllib.parse import urljoin, urlparse
+
+
 class Site:
-    def __init__(self,site,option):
+    def __init__(self,site):
         self.site = site
-        self.news = []
-        self.option = option
-        
+        self.news = {}
     def update_news(self):
-        if self.site.lower() == "economy":
-            if self.option == "news":
-                uf_url = f"https://www.infomoney.com.br/economia/"
-                browsers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome / 86.0.4240.198Safari / 537.36"}
-                page = requests.get(uf_url, headers = browsers)
-                soup = BeautifulSoup(page.content,"html.parser")
-                info = soup.find_all(class_="hover:underline")
-                dict_info = {}
-                for data in info:
-                    news = data.get_text(strip=True)
-                    news_link = data.get("href") 
-                    if len(news) >26:
-                        dict_info[news]=news_link
-                        
-                for k,v in dict_info.items():
-                    print(f"{k}:{v}\n")
-                self.news = dict_info
-        if self.site.lower() == "brazil":
-            if self.option == "life":
-                uf_url = f"https://www.cnnbrasil.com.br/nacional/brasil/"
-                browsers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome / 86.0.4240.198Safari / 537.36"}
-                page = requests.get(uf_url, headers = browsers)
-                soup = BeautifulSoup(page.content,"html.parser")
-                
-                life_h2 = soup.find_all(class_="text-gray-950 hover:underline")
-                life_h3 = soup.find_all(class_="flex w-fit hover:underline")
-                dict__options_life = {}
-                for new in life_h2:
-                    dict__options_life[new.h2.text] = new.get("href")
-                for new in life_h3:
-                    dict__options_life[new.h3.text] = new.get("href")
+        if self.site == "infomoney":
+            uf_url = f"https://www.infomoney.com.br/economia/"
+            browsers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome / 86.0.4240.198Safari / 537.36"}
+            page = requests.get(uf_url, headers = browsers)
+            soup = BeautifulSoup(page.content,"html.parser")
+            info = soup.find_all(class_="hover:underline")
+            dict_info = {}
+            for data in info:
+                news = data.get_text(strip=True)
+                news_link = data.get("href") 
+                if len(news) >26:
+                    dict_info[news]=news_link
                     
-                for k,v in dict__options_life.items():
-                    print(f"{k}:{v}\n")
-                self.news = dict__options_life
-            if self.option == "tech":
-                uf_url = f"https://www.cnnbrasil.com.br/tecnologia/"
-                browsers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome / 86.0.4240.198Safari / 537.36"}
-                page = requests.get(uf_url, headers = browsers)
-                soup = BeautifulSoup(page.content,"html.parser")
-                
-                tech_h3 = soup.find_all(class_="text-gray-950 hover:underline")
-                tech_h2 = soup.find_all(class_=" flex w-fit hover:underline")
-                dict__options_tech = {}
-                for i in tech_h2:
-                    dict__options_tech[i.h2.text] = i.get("href")
-                for i in tech_h3:
-                    dict__options_tech[i.h3.text] = i.get("href")
-                for k,v in dict__options_tech.items():
-                    print(f"{k}:{v}\n")
-               
-                self.news = dict__options_tech
-               
-        if self.site.lower() == "bbc":
-            if self.option == "news":
-                uf_url = f"https://www.bbc.com/news"
-                browsers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome / 86.0.4240.198Safari / 537.36"}
-                page = requests.get(uf_url, headers = browsers)
-                soup = BeautifulSoup(page.content,"html.parser")
-                
-                
-                dict__options_bbc_news = {}
-                
-                bbc_news_h2 = soup.find_all(class_="sc-8a623a54-0 hMvGwj")
-                for new_bbc in bbc_news_h2:
-                    h2 = new_bbc.text
-                    link = new_bbc.get("href")
-                    if len(h2) > 33:
-                        if "https://www.bbc.com/news" in link:
-                            dict__options_bbc_news[h2] = link
-                        else:
-                            dict__options_bbc_news[h2] = f"https://www.bbc.com/news{link}"
-                            
-                for k,v in dict__options_bbc_news.items():
-                    print(f"{k}:{v}\n")
-                self.news = dict__options_bbc_news
-        
-                        
-            if self.option == "tech":
-                uf_url = f"https://www.bbc.com/innovation"
-                browsers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome / 86.0.4240.198Safari / 537.36"}
-                page = requests.get(uf_url, headers = browsers)
-                soup = BeautifulSoup(page.content,"html.parser")
-                
-                
-                dict__options_bbc_tech = {}
-                
-                bbc_tech_h2 = soup.find_all(class_="sc-8a623a54-0 hMvGwj")
-                for new_bbc in bbc_tech_h2:
-                    h2 = new_bbc.text
-                    link = new_bbc.get("href")
-                    if len(h2) > 33:
-                        if "https://www.bbc.com/innovation" in link:
-                            dict__options_bbc_tech[h2] = link
-                        else:
-                            dict__options_bbc_tech[h2] = f"https://www.bbc.com/innovation{link}"
-                            
-                for k,v in dict__options_bbc_tech.items():
-                    print(f"{k}:{v}\n")
-                self.news = dict__options_bbc_tech
+            # for k,v in dict_info.items():
+            #     print(f"{k}:{v}\n")
+            self.news = dict_info
+        if self.site == "cnn_news":
+            uf_url = f"https://www.cnnbrasil.com.br/nacional/brasil/"
+            browsers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome / 86.0.4240.198Safari / 537.36"}
+            page = requests.get(uf_url, headers = browsers)
+            soup = BeautifulSoup(page.content,"html.parser")
             
-teste = Site(site="bbc",option="tech")
-teste.update_news()
+            life_h2 = soup.find_all(class_="text-gray-950 hover:underline")
+            life_h3 = soup.find_all(class_="flex w-fit hover:underline")
+            dict__options_life = {}
+            for new in life_h2:
+                dict__options_life[new.h2.text] = new.get("href")
+            for new in life_h3:
+                dict__options_life[new.h3.text] = new.get("href")
+                
+            # for k,v in dict__options_life.items():
+            #     print(f"{k}:{v}\n")
+                self.news = dict__options_life
+        if self.site == "cnn_tech":
+            uf_url = f"https://www.cnnbrasil.com.br/tecnologia/"
+            browsers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome / 86.0.4240.198Safari / 537.36"}
+            page = requests.get(uf_url, headers = browsers)
+            soup = BeautifulSoup(page.content,"html.parser")
+            
+            tech_h3 = soup.find_all(class_="text-gray-950 hover:underline")
+            tech_h2 = soup.find_all(class_=" flex w-fit hover:underline")
+            dict__options_tech = {}
+            for i in tech_h2:
+                dict__options_tech[i.h2.text] = i.get("href")
+            for i in tech_h3:
+                dict__options_tech[i.h3.text] = i.get("href")
+            # for k,v in dict__options_tech.items():
+            #     print(f"{k}:{v}\n")
+            
+            self.news = dict__options_tech    
+        if self.site == "bbc_news":
+            uf_url = f"https://www.bbc.com/news"
+            browsers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome / 86.0.4240.198Safari / 537.36"}
+            page = requests.get(uf_url, headers = browsers)
+            soup = BeautifulSoup(page.content,"html.parser")
+            
+            
+            dict__options_bbc_news = {}
+            
+            bbc_news_h2 = soup.find_all(class_="sc-8a623a54-0 hMvGwj")
+            for new_bbc in bbc_news_h2:
+                h2 = new_bbc.text
+                link = new_bbc.get("href")
+                if len(h2) > 33:
+                    if "https://www.bbc.com/news" in link:
+                        dict__options_bbc_news[h2] = link
+                    else:
+                        dict__options_bbc_news[h2] = f"https://www.bbc.com/news{link}"
+                        
+            # for k,v in dict__options_bbc_news.items():
+            #     print(f"{k}:{v}\n")
+            self.news = dict__options_bbc_news
+    
+                        
+        if self.site == "bbc_tech":
+            uf_url = f"https://www.bbc.com/innovation"
+            browsers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome / 86.0.4240.198Safari / 537.36"}
+            page = requests.get(uf_url, headers = browsers)
+            soup = BeautifulSoup(page.content,"html.parser")
+            
+            
+            dict__options_bbc_tech = {}
+            
+            bbc_tech_h2 = soup.find_all(class_="sc-8a623a54-0 hMvGwj")
+            for new_bbc in bbc_tech_h2:
+                h2 = new_bbc.text
+                link = new_bbc.get("href")
+                if len(h2) > 33:
+                    if "https://www.bbc.com/innovation" in link:
+                        dict__options_bbc_tech[h2] = link
+                    else:
+                        dict__options_bbc_tech[h2] = f"https://www.bbc.com/innovation{link}"
+                        
+            # for k,v in dict__options_bbc_tech.items():
+            #     print(f"{k}:{v}\n")
+            self.news = dict__options_bbc_tech
+        
+
 
    
 
